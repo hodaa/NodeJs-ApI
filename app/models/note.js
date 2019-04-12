ebdd33ef19da2d6fa16ecb138fda76f63c9127eb
Hoda <hhussin@mobiledoctors24-7.com>
2019-04-12 11:33:52 +0200

    push project

---
+++ app/models/note.js
@@ -0,0 +1,10 @@
+mongoose=require('mongoose');
+
+const NoteSchema = mongoose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
